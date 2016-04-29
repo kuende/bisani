a6f075ff6183cbf416850344cd52b4fdb55ba904
Teodor <teodor.pripoae@gmail.com>
2016-04-29 12:44:26 +0300

    Fix while loop to 100_000.times

--- spec/bisani_spec.cr
+++ spec/bisani_spec.cr
@@ -6,54 +6,50 @@ describe Bisani do
   it "obfuscate correctly i32" do
     seed = Random.new_seed
     rnd = Random.new
-    i = 1
-    while i <= 100_000
+
+    100_000.times do
       v = rnd.next_int.to_i32
       while v <= 0
         v = rnd.next_int.to_i32
       end
       r = Bisani.to_i32(v.to_bis)
       v.should eq(r)
-      i += 1
     end
   end
 
   it "obfuscate correctly i64" do
     seed = Random.new_seed
     rnd = Random.new(seed)
-    i = 1
-    while i <= 100_000
+
+    100_000.times do
       v = rnd.next_int.to_i64
       while v <= 0
         v = rnd.next_int.to_i64
       end
       r = Bisani.to_i64(v.to_bis)
       v.should eq(r)
-      i += 1
     end
   end
 
   it "obfuscate correctly ui32" do
     seed = Random.new_seed
     rnd = Random.new(seed)
-    i = 1
-    while i <= 100_000
+
+    100_000.times do
       v = rnd.next_u32
       r = Bisani.to_u32(v.to_bis)
       v.should eq(r)
-      i += 1
     end
   end
 
   it "obfuscate correctly ui64" do
     seed = Random.new_seed
     rnd = Random.new(seed)
-    i = 1
-    while i <= 100_000
+
+    100_000.times do
       v = rnd.next_u32.to_u64
       r = Bisani.to_u64(v.to_bis)
       v.should eq(r)
-      i += 1
     end
   end
 end
